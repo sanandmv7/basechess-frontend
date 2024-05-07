f0d8ea4247f0efed9eab8cdfa2de1b2c146d2094
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2024-05-07 19:50:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Web3Chess
+Just a fun little project.
+
+# Instructions
+
+npm install
+
+npm start
+
+## Start websocket server
+
+cd backend
+
+npm install
+
+node server.js
+
+## Deploy app to internet(Using Render)
+* Create an account in github
+* Create a repo to store backend of the project
+* Create a repo to store frontend of the project
+* Go to Render(https://render.com/) and sign up with your github account
+
+--------------------------------------------------
+### Backend:
+* Click on New + button in top right corner of Render dashboard and choose Web Service
+
+#### Deploy server.js
+* Build Command: npm install
+* Start Command: node server.js
+
+#### Deploy chatserver.js
+* Build Command: npm install
+* Start Command: node chatserver.js
+
+---------------------------------------------------
+* Update backend URLs in frontend src/utils/const.js
+* Update environment variables
+---------------------------------------------------
+
+### Frontend:
+* Click on New + button in top right corner of Render dashboard and choose Static Site
+
+* Build Command: npm run build
+* Publish directory: build
+
+
+## How to deploy on Vercel:
+* Go to Vercel(https://vercel.com/) and sign in/sign up with github account
+* Click on Add New -> Project from dashboard
+* Select web3chess frontend repository from Import Git Repository section
+
+## How to deploy on Netlify:
+* Go to Netlify(https://www.netlify.com/) and sign in/sign up with github account
+* From Team overview section, click on Add New Site -> Import andd existing project
+* Click on Deploy with GitHub
+* Select the repository
+* Fill in Site name and Add environment variable CI=False
+* Click on Deploy button
+
+## How to update the game smart contract address to your contract's address
+
+* Copy the contents of `Chess.sol` from the contracts folder
+* Go to Remix and create a new .sol file
+* Paste the contents
+* Compile and Deploy the contract to Polygon Testnet
+* Copy the deployed contract address
+* Go to src/build.eth/contracts/Chess.json file and paste the copied contract address under "networks":"80001":"address" attribute(or just search for the current address "0x8481ABc4f59F541a9610Ac93Eb5583F881AF9dB4" and replace it with new address)
---
+++ web3chess-frontend/contracts/Chess.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Chess {
+    uint256 public gameID;
+    address public owner;
+
+    uint256 public minWagerAmount = 0.01 ether;
+    uint256 public feePercentageBPS = 200; // 2% of bet amount
+
+    struct Game {
+        address player1;
+        address player2;
+        address payable winner;
+        uint256 amountBet;
+        bool finished;
+    }
+
+    mapping(uint256 => Game) public GameInfo;
+    mapping(address => uint256[]) public GamesPlayed;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        gameID = 1568;
+    }
+
+    function changeOwner(address newOwner) external onlyOwner {
+        owner = newOwner;
+    }
+
+    // newMinBetAmount should be in Wei
+    function changeMinBetAmount(uint256 newMinBetAmount) external onlyOwner {
+        minWagerAmount = newMinBetAmount;
+    }
+
+    function newGame() public payable {
+        require(msg.value >= minWagerAmount, "Invalid Bet Amount");
+
+        uint256 feeAmount = (msg.value * feePercentageBPS) / 10000;
+        payable(owner).transfer(feeAmount);
+
+        GameInfo[gameID].player1 = msg.sender;
+        GameInfo[gameID].amountBet = msg.value;
+        GameInfo[gameID].finished = false;
+        uint256 game = gameID;
+        GamesPlayed[msg.sender].push(game);
+        gameID++;
+    }
+
+    function getGameID(address player) public view returns (uint256) {
+        return GamesPlayed[player][GamesPlayed[player].length - 1];
+    }
+
+    function getStake(uint256 game) public view returns (uint256) {
+        return GameInfo[game].amountBet;
+    }
+
+    function joinGame(uint256 game) public payable {
+        require(GameInfo[game].amountBet != 0, "Game does not exist");
+        require(msg.value == GameInfo[game].amountBet, "Invalid Bet Amount");
+
+        uint256 feeAmount = (msg.value * feePercentageBPS) / 10000;
+        payable(owner).transfer(feeAmount);
+
+        GamesPlayed[msg.sender].push(game);
+        GameInfo[game].player2 = msg.sender;
+    }
+
+    function declareWinner(uint256 game, address winner) public onlyOwner {
+        require(!GameInfo[game].finished);
+        require(GameInfo[game].amountBet != 0, "Game does not exist");
+        require(GameInfo[game].winner == address(0));
+        require(
+            winner == GameInfo[game].player1 ||
+                winner == GameInfo[game].player2,
+            "Winner not Valid"
+        );
+        GameInfo[game].winner = payable(winner);
+        GameInfo[game].finished = true;
+
+        uint256 feeAmount = (GameInfo[game].amountBet * feePercentageBPS) /
+            10000;
+
+        (bool success, ) = GameInfo[game].winner.call{
+            value: (2 * (GameInfo[game].amountBet - feeAmount))
+        }("");
+        require(success, "Failed to send");
+    }
+
+    function returnWagers(uint256 gameId) public onlyOwner {
+        require(!GameInfo[gameId].finished);
+        require(GameInfo[gameId].amountBet != 0, "Game does not exist");
+        require(GameInfo[gameId].winner == address(0));
+        GameInfo[gameId].finished = true;
+        uint256 feeAmount = (GameInfo[gameId].amountBet * feePercentageBPS) /
+            10000;
+        
+        (bool success1, ) = GameInfo[gameId].player1.call{
+            value: (GameInfo[gameId].amountBet - feeAmount)
+        }("");
+        require(success1, "Failed to send to player 1");
+
+        (bool success2, ) = GameInfo[gameId].player2.call{
+            value: (GameInfo[gameId].amountBet - feeAmount)
+        }("");
+        require(success2, "Failed to send to player 2");
+    }
+
+    function getGames(address user) public view returns (uint256[] memory) {
+        uint256[] memory games = GamesPlayed[user];
+        return games;
+    }
+
+    function getLatestGameId() public view returns (uint256) {
+        return gameID;
+    }
+
+    function getGame(uint256 game) public view returns (Game memory) {
+        Game memory requestedGame;
+        requestedGame = GameInfo[game];
+        return requestedGame;
+    }
+}
